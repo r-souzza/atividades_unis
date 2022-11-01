9d4da631b75e435bf0900d5ac80264de5e67b619
r-souzza <rsouzza.2016@gmail.com>
2022-10-31 21:34:51 -0300

    Atividades

---
+++ PycharmProjects/Atividades/Atividade1.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+#-*- coding: latin 1 -*-
+
+idade = input("Insira sua idade: ")
+idadedias = idade * 365
+idademeses = idade * 12
+idadeanos = idade * 1
+print "Sua idade pode ser representada por", idadeanos , "anos,", idademeses, "meses ou ", idadedias, "dias"
---
+++ PycharmProjects/Atividades/Atividade2.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+#-*- coding: latin 1 -*-
+
+valor1 = input("Insira um valor: ")
+valor2 = input("Insira um segundo valor: ")
+valor3 = input("Insira um terceiro valor: ")
